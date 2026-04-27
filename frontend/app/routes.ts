18108e203d237639bd73280d4ae890c753f72177
Jung TaeSeung <mike415415@naver.com>
2026-04-27 22:08:55 +0900

    feat : callback route 구성

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -1,3 +1,10 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+    //index는 기본 라우트("/")로 사용될 컴포넌트를 지정할 때 사용
+    index("routes/home.tsx"),
+
+    // route("라우팅경로명", "라우팅할 실제 컴포넌트 파일 경로명"),
+    route("auth/github/callback", "routes/auth/callback.tsx"),
+
+] satisfies RouteConfig;
