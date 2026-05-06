673ba56f83d45e6c5e1f6bcf926f4b787c87d500
Jung TaeSeung <mike415415@naver.com>
2026-05-06 13:32:58 +0900

    feat : Repository List placeholder 구현

--- frontend/app/components/RepositoryList.tsx
+++ frontend/app/components/RepositoryList.tsx
@@ -12,10 +12,27 @@ export default function RepositoryList(){
     const { githubDataState, isLoading, isError } = useGithub<GithubRepositoryResponse>("/api/users/repos") as useGithubResult<GithubRepositoryResponse>;
 
     if(isLoading){
-        return <p className="text-xl">Loading repositories...</p>;
+        return <section>
+            <h2 className="text-xl font-semibold mb-4">Your Repositories</h2>
+              {[1,2,3,4].map(repo => {
+                return(
+                    <div className="bg-gray-900 border border-gray-800 rounded-lg p-4 mb-3 text-gray-50">
+                        <div rel="noopener noreferrer" className="text-lg font-bold hover:underline bg-gray-700 rounded animate-pulse w-3/4"> 
+                           &nbsp;
+                        </div>
+                        <p className="text-gray-400 text-sm mt-1 bg-gray-700 rounded animate-pulse w-1/3">&nbsp; </p>
+                        <div className="flex items-center gap-4 mt-2 text-gray-500 text-sm bg-gray-700 rounded animate-pulse w-2/3">
+                            <span>&nbsp;</span>
+                            <span>&nbsp;</span>
+                        </div>
+                        <div className="flex items-center gap-4 mt-2 text-gray-500 text-sm bg-gray-700 rounded animate-pulse w-1/3">
+                            &nbsp;
+                        </div>
+                    </div>
+                )
+            })}
+        </section>
     }
-
-    console.log("Repository Data State:", githubDataState);
 
     return (
         <section>
