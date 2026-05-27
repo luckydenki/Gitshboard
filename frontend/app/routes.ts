7f890a11f88d7cce4a8f8c7c7b276d8b4c30137b
Jung TaeSeung <mike415415@naver.com>
2026-05-28 01:35:04 +0900

    feat : frontend route.ts 추가

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -1,11 +1,15 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
+import { type RouteConfig, index, layout, route } from "@react-router/dev/routes";
 
 export default [
     //index는 기본 라우트("/")로 사용될 컴포넌트를 지정할 때 사용
     index("routes/home.tsx"),
-    // route("라우팅경로명", "라우팅할 실제 컴포넌트 파일 경로명"),
-    route("dashboard", "routes/dashboard.tsx"),
-    route("testpage", "routes/testpage.tsx"),
+    
+    layout("components/layout/DashboardLayout.tsx", [
+        route("dashboard", "routes/dashboard.tsx"),
+        route("testpage", "routes/testpage.tsx"),
+    ]),
+    //route("testpage", "routes/testpage.tsx"),
+
     route("auth/github/callback", "routes/auth/callback.tsx"),
 
 ] satisfies RouteConfig;
