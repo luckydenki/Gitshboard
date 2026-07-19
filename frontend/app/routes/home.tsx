cf9cba884add28089bc196e5ec7153614230b4bd
Jung TaeSeung <mike415415@naver.com>
2026-07-19 16:23:57 +0900

    chore : home.tsx 쓸모 없는 문구 제거

--- frontend/app/routes/home.tsx
+++ frontend/app/routes/home.tsx
@@ -7,35 +7,6 @@ import SearchForm from "~/components/page/home/SearchForm";
 import LoginButton from "~/components/page/home/LoginButton";
 import { isLocal} from '~/utils/log_system/log';
 
-/** 임시 tailwind css 작성 규칙
- * 
- * 1. 요소가 놓일 포지션 위치와 관련된 클래스
- * ex) flex, grid, absolute, top-0, left-0, items-center, gap
- * 
- * 2. 요소의 크기와 관련된 클래스
- * ex) w-1/2, h-12, min-w-md, max-w-4xl, p-6, m-4
- * 
- * 3. 배경, 테두리, 그림자 관련 클래스
- * ex) bg-gray-300, bg-white, border, border-gray-300, shadow-md
- * 
- * 4. 글자, 폰트 관련 클래스
- * ex) text-sm, text-lg, font-bold, font-medium, font-light...
- * 
- * 5. 상태 관련 클래스
- * ex) hover:bg-gray-200, focus:outline-none, active:ring-2....
- * 
- * 6. 반응형 관련 클래스
- * ex) sm:text-sm, md:text-lg, lg:text-xl, not-sm:text-sm, not-md:text-lg...
- * 
- * 7. animation, transition 관련 클래스, 만약 css로 직접 만든 애니메이션이 있다면 여기에 작성.
- * ex) animate-pulse, transition-all, duration-200, ease-in-out...
- * 
- * 8. 그외 기타 커스텀 css, 또는 위에 해당하지 않는 모든 클래스
- * 
- * 
- */
-
-
 
 export function meta({}: Route.MetaArgs) {
   return [
