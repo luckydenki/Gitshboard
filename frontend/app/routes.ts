6cb378432fbc7333b505a52d13805c51304248fd
Jung TaeSeung <mike415415@naver.com>
2026-07-19 17:18:52 +0900

    feat :search용  queryclientprovider layout 추가

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -3,7 +3,10 @@ import { type RouteConfig, index, layout, route } from "@react-router/dev/routes
 export default [
     //index는 기본 라우트("/")로 사용될 컴포넌트를 지정할 때 사용
     index("routes/home.tsx"),
-    route("search","routes/search.tsx"),
+    layout("components/layout/QueryClientLayout.tsx", [
+            route("search","routes/search.tsx"),
+    ]
+    ),
     layout("components/layout/DashboardLayout.tsx", [
         route("dashboard", "routes/dashboard.tsx"),
         route("testpage", "routes/testpage.tsx"),
--- frontend/app/routes/search.tsx
+++ frontend/app/routes/search.tsx
@@ -1,4 +1,7 @@
+import { QueryClient, useQuery } from "@tanstack/react-query";
+import { useState } from "react";   
 import { useSearchParams } from "react-router"
+import getBackendURL from "~/utils/getBackendURL";
 
 
 
@@ -6,14 +9,34 @@ import { useSearchParams } from "react-router"
 export default function Search() {
 
     const [searchParams, setSearchParams] = useSearchParams();
+    const searchName : string = searchParams.get("search_name") ?? "";
 
-    const searchName = searchParams.get("search_name");
+    const [queryClient] = useState(()=>new QueryClient()); //이렇게 하면 컴포넌트가 처음 렌더링 될 때 한 번만 생성되고 이후에는 같은 인스턴스를 사용합니다.
+
 
     console.log("Search page loaded with search_name:", searchName);
 
+    const { data, isLoading, isError} = useQuery(
+         {
+            queryKey : ["search", searchName],
+            queryFn : async ()=>{
+                const backendURL = getBackendURL();
+                const res = await fetch(`${backendURL}/search?search_name=${encodeURIComponent(searchName)}`);
+                return res.json();
+            },
+            staleTime : 5* 60 * 1000, // 5분
+            gcTime : 10 * 60 * 1000, // 10분
+            enabled : !!searchName, // searchName이 존재할 때만 쿼리 실행
+         }
+    );
+
+
+
     return(
         <>
-            
+
+
+
         </>
     )
 }
