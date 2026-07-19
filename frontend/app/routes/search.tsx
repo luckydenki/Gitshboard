08fb8943c06e2fd3492d31a8ff2ddca8d6412d5a
Jung TaeSeung <mike415415@naver.com>
2026-07-19 22:45:31 +0900

    feat : frontend 일반 searching api 추가 및 기존 레이아웃 디자인

--- frontend/app/routes/search.tsx
+++ frontend/app/routes/search.tsx
@@ -1,42 +1,81 @@
-import { QueryClient, useQuery } from "@tanstack/react-query";
-import { useState } from "react";   
+import { useQuery } from "@tanstack/react-query";
 import { useSearchParams } from "react-router"
+import type { CommonErrorResponse, CommonResponse, ErrorStatus, SuccessStatus } from "~/types/common/common";
 import getBackendURL from "~/utils/getBackendURL";
 
-
+interface GithubUserSearchResponse{
+    total_count: number
+    incomplete_results: boolean,
+    items: Array<{
+        login: string,
+        id: number,
+        avatar_url: string,
+        html_url: string,
+        type: string
+    }>
+}
 
 
 export default function Search() {
 
     const [searchParams, setSearchParams] = useSearchParams();
-    const searchName : string = searchParams.get("search_name") ?? "";
+    const name : string = searchParams.get("search_name") ?? "";
 
-    const [queryClient] = useState(()=>new QueryClient()); //이렇게 하면 컴포넌트가 처음 렌더링 될 때 한 번만 생성되고 이후에는 같은 인스턴스를 사용합니다.
-
-
-    console.log("Search page loaded with search_name:", searchName);
+    console.log("Search page loaded with search_name:", name);
 
     const { data, isLoading, isError} = useQuery(
          {
-            queryKey : ["search", searchName],
+            queryKey : ["search", name],
             queryFn : async ()=>{
                 const backendURL = getBackendURL();
-                const res = await fetch(`${backendURL}/search?search_name=${encodeURIComponent(searchName)}`);
-                return res.json();
+                console.log("backendURL:", backendURL);
+                try{
+                    const res = await fetch(`${backendURL}/api/search?name=${encodeURIComponent(name)}`);
+                    if(res.ok){
+                        const data : CommonResponse<GithubUserSearchResponse> = await res.json();
+                        return data.data;
+                    }
+                    else{
+                        const errorData : CommonErrorResponse = await res.json();
+                        throw new Error(`Error ${errorData.status}: ${errorData.title} - ${errorData.detail}`);
+                    }
+                }catch(error){
+                    console.error("Failed to fetch search results:", error);
+                }
+
             },
             staleTime : 5* 60 * 1000, // 5분
             gcTime : 10 * 60 * 1000, // 10분
-            enabled : !!searchName, // searchName이 존재할 때만 쿼리 실행
+            enabled : !!name, // name이 존재할 때만 쿼리 실행
          }
     );
 
 
+    console.log("frontend data" ,data);
+
+    if(isLoading){
+        return <div>Loading...</div>
+    }
 
     return(
-        <>
+        <div className="flex flex-col h-full items-center justify-center">
 
-
-
-        </>
+            <header> 
+                <h1>Search Results for "{name}"</h1>
+            </header>
+            <section>
+                {data?.items.map((user)=>{
+                    return(
+                        <article key={user.id} className="flex ">
+                            <a href={user.html_url} target="_blank" rel="noopener noreferrer">
+                            <img src={user.avatar_url} alt={`${user.login}'s avatar`} width={50} height={50}/>
+                            <span>{user.login}</span>
+                            </a>
+                        </article>
+                    )
+                })}
+            </section>
+            
+        </div>
     )
 }
