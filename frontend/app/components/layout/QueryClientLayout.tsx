377059f67a0d10a6014aea371dfba291dec4be85
Jung TaeSeung <mike415415@naver.com>
2026-07-19 20:25:49 +0900

    fix : frontend QueryClientLayout Outlet 주입으로 변경

--- frontend/app/components/layout/QueryClientLayout.tsx
+++ frontend/app/components/layout/QueryClientLayout.tsx
@@ -1,17 +1,18 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
+import { Outlet } from "react-router";
 
 
 
 
 
-export default function QueryClientLayout({ children }: { children: React.ReactNode }) {
+export default function QueryClientLayout() {
 
     const [queryClient] = useState(()=>new QueryClient()); //이렇게 하면 컴포넌트가 처음 렌더링 될 때 한 번만 생성되고 이후에는 같은 인스턴스를 사용합니다.
 
     return(
         <QueryClientProvider client ={queryClient}>
-            {children}
+            <Outlet/>
         </QueryClientProvider>
 
     )
