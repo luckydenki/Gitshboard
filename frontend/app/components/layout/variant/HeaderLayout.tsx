8df7aae57f7fe4bed28d6ee773484cee43d93b48
Jung TaeSeung <mike415415@naver.com>
2026-07-20 16:56:24 +0900

    feat : HeaderLayout onClick추가

--- frontend/app/components/layout/variant/HeaderLayout.tsx
+++ frontend/app/components/layout/variant/HeaderLayout.tsx
@@ -3,16 +3,12 @@ import SideTitleLogo from "../../design/SideTitleLogo";
 
 
 
-export default function HeaderLayout({children} : {children? : React.ReactNode}){
-
-    const navigate = useNavigate();
+export default function HeaderLayout({children, onClick} : {children? : React.ReactNode, onClick : ()=>void}){
 
     return(
         <header className="sticky top-0 z-10 bg-white/80 px-6 py-4 shadow-[0_12px_40px_rgba(15,23,42,0.08)] backdrop-blur-xl dark:bg-gray-950/70">
             <div className="mx-auto flex max-w-360 items-center justify-between">
-                <SideTitleLogo onClick={()=>{
-                    navigate("/dashboard");
-                }}/>
+                <SideTitleLogo onClick={onClick}/>
                  {children}
             </div>
         </header>
