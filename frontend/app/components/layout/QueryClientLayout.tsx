cda059c0b4049fa6065def012c5fca340cd46843
Jung TaeSeung <mike415415@naver.com>
2026-07-20 16:56:04 +0900

    feat : DashboardHeader onClick 추가

--- frontend/app/components/layout/QueryClientLayout.tsx
+++ frontend/app/components/layout/QueryClientLayout.tsx
@@ -1,6 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
-import { Outlet } from "react-router";
+import { Outlet, useNavigate } from "react-router";
 import HeaderLayout from "./variant/HeaderLayout";
 
 
@@ -10,10 +10,12 @@ import HeaderLayout from "./variant/HeaderLayout";
 export default function QueryClientLayout() {
 
     const [queryClient] = useState(()=>new QueryClient()); //이렇게 하면 컴포넌트가 처음 렌더링 될 때 한 번만 생성되고 이후에는 같은 인스턴스를 사용합니다.
+    const navigate = useNavigate();
 
     return(
         <QueryClientProvider client ={queryClient}>
-            <HeaderLayout/>
+            <HeaderLayout onClick={()=>{navigate("/")}}
+                />
             <Outlet/>
         </QueryClientProvider>
 
