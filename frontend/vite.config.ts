ec16192f8c46edb0884d6b114019552a168874ee
Jung TaeSeung <mike415415@naver.com>
2026-07-25 00:05:29 +0900

    config : vite config json proxy 설정

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,6 +7,12 @@ export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
   server: {          
     host : "0.0.0.0",
-    port : 5173
+    port : 5173,
+    proxy : {
+      "/api" : {
+        target: "http://localhost:3000",
+        changeOrigin :true
+      }
+    }
   }
 });
