a0687d11d92c55da2f9ec200f72bd6e9944175ea
Jung TaeSeung <mike415415@naver.com>
2026-07-30 00:05:57 +0900

    config : gitignore 추가, vite 설정 정상화

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,8 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
-import { fileURLToPath } from "node:url";
-import path from "node:path";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-const dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // USE_VITE_PROXY=false npx vercel dev --listen 5173
 
@@ -17,9 +14,6 @@ export default defineConfig(() => {
   return {
     plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
     resolve: {
-      alias: {
-        "~": path.resolve(dirname, "app"),
-      },
     },
     server: {          
       host : "0.0.0.0",
