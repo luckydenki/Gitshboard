327f094423153ba37bc600725054ff7358029276
Jung TaeSeung <mike415415@naver.com>
2026-07-29 16:14:35 +0900

    config : frontend vitest storybook 설정 추가

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from "vitest/config";
-// import path from 'node:path';
-// import { fileURLToPath } from 'node:url';
-// import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
-// import { playwright } from '@vitest/browser-playwright';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
+import { playwright } from '@vitest/browser-playwright';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 
-// const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
+const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
@@ -21,26 +21,26 @@ export default defineConfig({
       }
     },
 
-    // {
-    //   extends: true,
-    //   plugins: [
-    //   // The plugin will run tests for the stories defined in your Storybook config
-    //   // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
-    //   storybookTest({
-    //     configDir: path.join(dirname, '.storybook')
-    //   })],
-    //   test: {
-    //     name: 'storybook',
-    //     browser: {
-    //       enabled: true,
-    //       headless: true,
-    //       provider: playwright({}),
-    //       instances: [{
-    //         browser: 'chromium'
-    //       }]
-    //     }
-    //   }
-    // }
+    {
+      extends: true,
+      plugins: [
+      // The plugin will run tests for the stories defined in your Storybook config
+      // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+      storybookTest({
+        configDir: path.join(dirname, '.storybook')
+      })],
+      test: {
+        name: 'storybook',
+        browser: {
+          enabled: true,
+          headless: true,
+          provider: playwright({}),
+          instances: [{
+            browser: 'chromium'
+          }]
+        }
+      }
+    }
 
 
   ]
