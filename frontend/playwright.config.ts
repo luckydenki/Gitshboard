a3d7e4bf55f1c7a4b36bc889672fac6a70d3697f
Jung TaeSeung <mike415415@naver.com>
2026-07-31 21:28:22 +0900

    config : 개 거슬리는 playwright 타스 타입 에러 및 playwright 테스트 생성 위치 변경

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -5,11 +5,21 @@ const useExternalServer = Boolean(process.env.PLAYWRIGHT_BASE_URL);
 
 export default defineConfig({
   testDir: "./e2e",
+  outputDir: './e2e/test-results',
+  // HTML 리포트 저장 위치
+  reporter: [
+    [
+      'html',
+      {
+        outputFolder: './e2e/playwright-report/html',
+        open: 'never',
+      },
+    ],
+  ],
   fullyParallel: true,
   forbidOnly: Boolean(process.env.CI),
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: "html",
   use: {
     baseURL,
     trace: "on-first-retry",
