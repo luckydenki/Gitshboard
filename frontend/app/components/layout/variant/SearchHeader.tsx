e6ea906f641f70d1eca2cb0604180fcbdad73ae7
Jung TaeSeung <mike415415@naver.com>
2026-08-20 19:16:00 +0900

    refactor : searchHeader에 변경된 authcheck 로직 추가

--- frontend/app/components/layout/variant/SearchHeader.tsx
+++ frontend/app/components/layout/variant/SearchHeader.tsx
@@ -2,21 +2,20 @@ import HeaderLayout from "./HeaderLayout";
 import LoginButton from "~/components/page/home/LoginButton";
 import useAuthCheck from "~/hooks/useAuthCheck";
 import ProfileButton from "./ProfileButton";
-import { Log } from "~/utils/log_system/log";
+
 
 
 export default function SearchHeader(){
     const ID =  import.meta.env.VITE_GITHUB_CLIENT_ID;
     const URL = import.meta.env.VITE_GITHUB_CALLBACK_URL;
-    const auth = useAuthCheck();
- 
-    Log("auth ", auth);
+    const { data, isLoading, isError, error } = useAuthCheck("dashboard");
+
     return(
         <HeaderLayout href="/">
-            {!auth.loginCheckState ? (
-                <LoginButton ID={ID} URL={URL} mode={"Tiny"}/>
-                ):(
+            {data && data.success ? (
                 <ProfileButton/>
+                ):(
+                <LoginButton ID={ID} URL={URL} mode={"Tiny"}/>
                 )
             }
         </HeaderLayout>
