d361f322fe8a0392e795043312568c1810984273
Jung TaeSeung <mike415415@naver.com>
2026-08-24 18:04:36 +0900

    refactor dashboard api fetch를 tanstackquery로 변경

---
+++ frontend/app/hooks/pages/dashboard-hooks.tsx
@@ -0,0 +1,28 @@
+import { useQuery } from "@tanstack/react-query";
+import type { GithubRepositoryResponse } from "~/types/GithubInfo";
+
+
+export function useDashboardData(){
+
+    const { data, isLoading, isError} = useQuery<GithubRepositoryResponse>({
+        queryKey: ["dashboardData"],
+        queryFn : async()=>{
+            const response = await fetch("/api/users/repos", {
+                method :'GET',
+                credentials : 'include'
+            });
+            
+            const json = await response.json();
+
+            if(!response.ok){
+                throw json;
+            }
+
+            console.log("dashboard data : ", json);
+            return json;
+        },
+    });
+
+    return { data, isLoading, isError};
+}
+
