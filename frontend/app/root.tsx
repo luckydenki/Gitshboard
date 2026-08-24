00eadc933e323c42ca4a181f6373ee4904a38593
Jung TaeSeung <mike415415@naver.com>
2026-08-24 18:05:03 +0900

    refactor : queryclient 에 공통 설정 적용

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -1,7 +1,7 @@
 import {
   isRouteErrorResponse,
-  Links,
   Meta,
+  Links,
   Outlet,
   Scripts,
   ScrollRestoration,
@@ -12,6 +12,7 @@ import { useRef, useState } from "react";
 
 import type { Route } from "./+types/root";
 import "./app.css";
+import { commonRetry } from "./utils/tanstackUtil";
 
 
 /**
@@ -77,20 +78,32 @@ export default function App() {
 
 
   const [queryClient] = useState(()=>new QueryClient({
+    defaultOptions : {
+      queries : {
+        staleTime : 1 * 60 * 1000, //1분
+        retry : commonRetry
+      },
+      mutations : {
+        retry : commonRetry
+      }
+    },
     queryCache : new QueryCache({
 
       onError : (error : any)=>{
         if('status' in error){
-
           //401 에러이면서 현재 위치가 home 화면만 아니면 됨
             if( error.status  === 401 && window.location.pathname !== "/" && !handle_401_Ref.current){
               handle_401_Ref.current = true;
-              alert("로그인 해주세요.");
+              alert("다시 로그인 해주세요.");
               navigate("/", { replace : true });
             }
         }
-      }
 
+        //CommonErrorResponse 타입이 아닌 완전한 예외 상황, 보통 네트워크 에러인 경우일 가능성 농후
+        else{
+            throw error;
+        }
+      }
     })
 
   })); //이렇게 하면 컴포넌트가 처음 렌더링 될 때 한 번만 생성되고 이후에는 같은 인스턴스를 사용합니다.
