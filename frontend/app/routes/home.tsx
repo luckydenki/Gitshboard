a66c3db20f38101a473b421a51b0c770431ca517
Jung TaeSeung <mike415415@naver.com>
2026-08-27 18:33:08 +0900

    fix : useAuthCheck가 두번 발생하던 원흉삭제

--- frontend/app/routes/home.tsx
+++ frontend/app/routes/home.tsx
@@ -17,7 +17,7 @@ export function meta({}: Route.MetaArgs) {
 
 
 export default function Home() {
-  const { isSuccess } = useAuthCheck("home");
+  const { isSuccess } = useAuthCheck();
 
 
   if(isSuccess){
