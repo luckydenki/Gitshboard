7399a2e550319903d1f6a0ac554a766fba0a8efc
Jung TaeSeung <mike415415@naver.com>
2026-08-27 18:29:53 +0900

    refactor : useAuthCheck훅 return변경 및 useManageKeyQuery 사용

--- frontend/app/hooks/useAuthCheck.tsx
+++ frontend/app/hooks/useAuthCheck.tsx
@@ -1,7 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 import type { CommonErrorResponse, CommonResponse } from "~/types/common/common";
-import { commonRetry } from "~/utils/tanstackUtil";
-
+import CommonError from "~/utils/common-error";
+import { commonRetry } from "~/utils/tanstack-utils";
+import { useState } from "react";
+import { QueryKeys } from "~/types/query-key-enum";
+import useManageKeyQuery from "./useManageKeyQuery";
 
 
 
@@ -10,34 +13,49 @@ import { commonRetry } from "~/utils/tanstackUtil";
 /**
  * 인증 상태인지 확인하는 훅입니다.
  * 
+ * - 성공시 : { success: true, status: 200, data: '인증된 사용자입니다.' }
+ * - 실패시 : { success: false, status: 200, data: null }
+ * 
+ * 따라서 해당 훅은 인증이 되어있는지 검증만 하고 싶을 때 사용하세요.
+ * 그 외 네트워크 에러는 500 에러로 throw됩니다.
  * 
  * @returns { data: CommonResponse<string> | undefined, isLoading: boolean, isError: boolean }
  */
-export default function useAuthCheck(keyword?: string){
+export default function useAuthCheck(){
 
-    const { data, isLoading, isError, error} = useQuery<CommonResponse<string>, CommonErrorResponse>({
-      queryKey : ['auth_check', keyword],
+    const [_, setTrigger] = useState(false);  
+
+    const { data, isLoading, isFetching, isError, error } = useManageKeyQuery([QueryKeys.AUTH_CHECK],{
       queryFn : async()=>{
-        try{
-          const res = await fetch(`/api/auth/check`,{
-            method : 'GET',
-            credentials : 'include'
-          })
+              try{
+                const res = await fetch(`/api/auth/check`,{
+                  method : 'GET',
+                  credentials : 'include'
+                })
 
-          const data : CommonResponse<string> = await res.json();
-          return data;
-          
-        }
-        catch(error : CommonErrorResponse | unknown){
-          throw error;
-        }
-      },
-      staleTime : 1,
-      retry : commonRetry,
+                const data : CommonResponse<string> = await res.json();
+                return data;
+                
+              }
+              catch(error : unknown){
+                throw new CommonError(
+                  {
+                    status : 500,
+                    title : "Network Error",
+                    type : "NetworkError",
+                    detail : "Network error occurred while checking authentication status."
+                  }
+                )
+              }
+            },
+            staleTime : 30 * 1000, //30초
+            retry : commonRetry,
+            refetchOnWindowFocus : 'always', 
     })
+  
 
 
-    return { data, isLoading, isError, error};
+    return { isSuccess: data?.success, isLoading, isFetching, isError, error, refetch : () => setTrigger(prev => !prev) };
     
     
 }
