c8bd48943ba7e069ab8f3ca4a80e23d53662212e
Jung TaeSeung <mike415415@naver.com>
2026-08-29 18:34:32 +0900

    docs  :CommonError 클래스 설명 문구 추가

--- backend/src/utils/common-error.ts
+++ backend/src/utils/common-error.ts
@@ -1,5 +1,14 @@
 import { CommonErrorResponse, ErrorStatus } from "../types/middlewares/common";
 
+/**
+ *  해당 클래스는 CommonErrorResponse 인터페이스를 구현하며, 공통적인 오류 정보를 담는 Error 객체를 생성합니다.
+ * 
+ * - status: HTTP 상태 코드 (예: 400, 404, 500 등)
+ * - title: 오류 제목 (예: "Bad Request", "Not Found", "Internal Server Error" 등)
+ * - type: 오류 유형을 나타내는 URI (예: "https://docs.github.com/en/graphql/overview/explorer")
+ * - detail: 오류에 대한 상세 설명 (선택 사항)
+ * - instance: 오류가 발생한 인스턴스 URI (선택 사항)
+ */
 class CommonError extends Error implements CommonErrorResponse {
     status: ErrorStatus;
     title: string;
