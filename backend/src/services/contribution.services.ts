80e35d30019c8ab44cc13572dca536557d33c6b4
Jung TaeSeung <mike415415@naver.com>
2026-08-29 17:25:42 +0900

    chore : 쓸데없는 import 삭제 및 error 처리 추가

--- backend/src/services/contribution.services.ts
+++ backend/src/services/contribution.services.ts
@@ -1,7 +1,8 @@
 import { CommitContributionActivity } from "../client/contribution.client";
 import contributionClient from "../client/contribution.client";
-import { GithubCommitActivity } from "../utils/contribution";
-
+import { redisClient } from "../infra/redis/redisClient";
+import CommonError from "../utils/common-error";
+import { getGithubCommitActivity } from "../utils/contribution";
 
 
 
@@ -13,32 +14,69 @@ class ContributionService {
     public getCommitActivity = async (githubAccessToken: string | undefined, githubUsername: string | undefined, from: string, to: string) => {
 
         try {
-            if (!githubAccessToken || !githubUsername) {
-                throw new Error('인증된 사용자가 아닙니다.');
+            if(from > to){
+                throw new CommonError (
+                    {
+                        status : 400,
+                        title : "Bad Request",
+                        type : "https://docs.github.com/en/graphql/overview/explorer",
+                        detail : "The 'from' date must be earlier than the 'to' date.",
+                        instance: "/graphql"
+                    }
+                )
+            }
+
+            const cachedData = await redisClient.get(`commitActivity:${githubUsername}:${from}-${to}`);
+            if (cachedData) {
+                console.log("Serving cached commit activity data");
+                return JSON.parse(cachedData);
+            }
+
+            if (!githubUsername) {
+                console.error("Missing required query parameter: username");
+                throw new CommonError({
+                    status: 400,
+                    title: "Bad Request",
+                    type: "https://docs.github.com/en/graphql/overview/explorer",
+                    detail: "Missing required query parameter: username",
+                    instance: "/graphql"
+                });
             }
             const data: CommitContributionActivity = await contributionClient.getCommitActivity(githubAccessToken, githubUsername, from, to);
-            
-            const githubCommitActivity = GithubCommitActivity(data);
-            
-            
+            const githubCommitActivity = getGithubCommitActivity(data);
+
+            //redis의 fromto는 yyyymmdd만 사용한다.
+            const redisFrom = from.split("T")[0].replace(/-/g, '');
+            const redisTo = to.split("T")[0].replace(/-/g, '');
+            redisClient.setEx(`commitActivity:${githubUsername}:${redisFrom}${redisTo}`, 60 * 60, JSON.stringify(githubCommitActivity)); // 캐시 만료 시간: 1시간
+
             return githubCommitActivity;
 
         }
         catch (error) {
             console.error("Error in /commitActivity route:", error);
 
-            if (error instanceof Error) {
-                const errorResponse = {
+            if(error instanceof CommonError){
+                throw error;
+            }
+            else if (error instanceof Error) {
+                throw new CommonError({
                     type: "https://docs.github.com/en/graphql/overview/explorer",
                     title: "GitHub API Error",
                     status: 500,
                     detail: error.message,
                     instance: "/graphql"
-                }
-                throw error;
+                });
             }
-
-            throw error;
+            else{
+                throw new CommonError({
+                    type: "https://docs.github.com/en/graphql/overview/explorer",
+                    title: "GitHub API Error",
+                    status: 500,
+                    detail: "An unexpected error occurred",
+                    instance: "/graphql"
+                });
+            }
         }
 
     }
