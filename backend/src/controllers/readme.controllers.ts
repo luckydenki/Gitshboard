6cf3122313404010a20b2533b342435ec30c8eb0
Jung TaeSeung <mike415415@naver.com>
2026-08-31 16:49:56 +0900

    refactor : readme api들 controller와 service로 분리

--- backend/src/controllers/readme.controllers.ts
+++ backend/src/controllers/readme.controllers.ts
@@ -2,10 +2,7 @@ import { Request, Response } from 'express';
 import CommonError from '../utils/common-error';
 import ErrorSVG from '../render/ErrorSVG';
 import readmeService from '../services/readme.services';
-import repoService from '../services/repo.services';
-import RenderTechnologyDistributionSVG from '../render/RenderTechnologyDistributionSVG';
-import RenderPreferredCommitTimeSVG from '../render/RenderPreferredCommitTimeSVG';
-import RenderWeeklyActivitySVG from '../render/RenderWeeklyActivitySVG';
+import { readmeErrorResponseHandler } from '../utils/readme.util';
 
 
 class ReadmeController {
@@ -19,94 +16,35 @@ class ReadmeController {
             const reqHeight = Number(req.query.height ?? 430);
 
             if (!username || username.trim() === "") {
-                throw new CommonError({
-                    status: 400,
-                    title: "Bad Request",
-                    type: "https://docs.github.com/en/graphql/overview/explorer",
-                    detail: "Github에서 사용하는 유저 이름을 username 쿼리 파라미터로 전달해 주세요",
-                    instance: "/api/readme/commit-activity.svg"
-                });
+                throw CommonError.create400Error("Github에서 사용하는 유저 이름을 username 쿼리 파라미터로 전달해 주세요", "/api/readme/commit-activity.svg");
             }
-
             const svg = await readmeService.commitActivity(username, reqWidth, reqHeight, reqFrom, reqTo);
-
             res.status(200).type('image/svg+xml').send(svg);
         }
         catch (err) {
-            let commonError;
-
-            if (err instanceof CommonError) {
-                commonError = err;
-            }
-            else if (err instanceof Error) {
-                commonError = new CommonError({
-                    status: 500,
-                    title: "Internal Server Error",
-                    type: "https://docs.github.com/en/graphql/overview/explorer",
-                    detail: err.message,
-                    instance: "/api/readme/commit-activity.svg"
-                });
-            }
-            else {
-                commonError = new CommonError({
-                    status: 500,
-                    title: "Internal Server Error",
-                    type: "https://docs.github.com/en/graphql/overview/explorer",
-                    detail: "An unexpected error occurred",
-                    instance: "/api/readme/commit-activity.svg"
-                });
-            }
-
-            res.status(commonError.status).type('image/svg+xml').send(ErrorSVG(commonError, 900, 430));
+            readmeErrorResponseHandler(err, res);
         }
     }
 
 
     public techDistribution = async (req : Request, res : Response) => {
         try {
-
             const username: string = String(req.query.username ?? "");
             const reqWidth = Number(req.query.width ?? 900);
             const reqHeight = Number(req.query.height ?? 430);
 
-            const languageStats = await repoService.getLanguages(username, undefined);
-
-            if (!languageStats) {
-                throw new CommonError({
-                    status: 500,
-                    title: "GitHub API Error",
-                    type: "https://docs.github.com/en/graphql/overview/explorer",
-                    detail: "레포지토리 언어 사용량 정보를 가져오는 데 실패했습니다.",
-                });
+            if (!username || username.trim() === "") {
+                throw CommonError.create400Error("Github에서 사용하는 유저 이름을 username 쿼리 파라미터로 전달해 주세요", "/api/readme/tech-distribution.svg");
             }
 
-            const svg = RenderTechnologyDistributionSVG(languageStats, reqWidth, reqHeight);
-            res.type('image/svg+xml').send(svg);
+            const svg = await readmeService.techDistribution(username, reqWidth, reqHeight);
+            res.status(200).type('image/svg+xml').send(svg);
+
         } catch (error) {
-            if (error instanceof CommonError) {
-                res.status(error.status).type('image/svg+xml').send(ErrorSVG(error, 900, 430));
-            }
-            else if (error instanceof Error) {
-                const commonError = new CommonError({
-                    type: "https://docs.github.com/en/graphql/overview/explorer",
-                    title: error.name,
-                    status: 500,
-                    detail: error.message
-                });
-                res.status(500).type('image/svg+xml').send(ErrorSVG(commonError, 900, 430));
-            }
-            else {
-                const error = new CommonError({
-                    type: "https://docs.github.com/en/graphql/overview/explorer",
-                    title: "Unknown Error",
-                    status: 500,
-                    detail: "An unknown error occurred."
-                });
-
-                res.status(500).type('image/svg+xml').send(ErrorSVG(error, 900, 430));
-            }
+            readmeErrorResponseHandler(error, res);
         }
     }
+    
 
     public preferredCommitTime = async (req: Request, res: Response) => {
         try {
@@ -114,137 +52,38 @@ class ReadmeController {
             const reqWidth = Number(req.query.width ?? 900);
             const reqHeight = Number(req.query.height ?? 430);
 
-            const data = await repoService.getCommitTime(username, undefined);
-
-            if (!data) {
-                throw new CommonError({
-                    status: 500,
-                    title: "GitHub API Error",
-                    type: "https://docs.github.com/en/graphql/overview/explorer",
-                    detail: "커밋 시간 정보를 가져오는 데 실패했습니다.",
-                });
+            if (!username || username.trim() === "") {
+                throw CommonError.create400Error("Github에서 사용하는 유저 이름을 username 쿼리 파라미터로 전달해 주세요", "/api/readme/preferred-commit-time.svg");
             }
 
-            const svg = RenderPreferredCommitTimeSVG(data, reqWidth, reqHeight);
-            res.type('image/svg+xml').send(svg);
+            const svg = await readmeService.preferredCommitTime(username, reqWidth, reqHeight);
+            res.status(200).type('image/svg+xml').send(svg);
 
         } catch (error) {
-            if (error instanceof CommonError) {
-                res.status(error.status).type('image/svg+xml').send(ErrorSVG(error, 900, 430));
-            }
-            else if (error instanceof Error) {
-                const commonError = new CommonError({
-                    type: "https://docs.github.com/en/graphql/overview/explorer",
-                    title: error.name,
-                    status: 500,
-                    detail: error.message
-                });
-                res.status(500).type('image/svg+xml').send(ErrorSVG(commonError, 900, 430));
-            }
-            else {
-                const error = new CommonError({
-                    type: "https://docs.github.com/en/graphql/overview/explorer",
-                    title: "Unknown Error",
-                    status: 500,
-                    detail: "An unknown error occurred."
-                });
-                res.status(500).type('image/svg+xml').send(ErrorSVG(error, 900, 430));
-            }
+            readmeErrorResponseHandler(error, res);
         }
     }
-
-    public weeklyActivity = async (req : Request, res : Response) => {
-        try {
-            const username: string = String(req.query.username ?? "");
-            const reqWidth = Number(req.query.width ?? 900);
-            const reqHeight = Number(req.query.height ?? 430);
-
-            const data = await repoService.getCommitTime(username, undefined);
-
-            if (!data) {
-                throw new CommonError({
-                    status: 500,
-                    title: "GitHub API Error",
-                    type: "https://docs.github.com/en/graphql/overview/explorer",
-                    detail: "주간 커밋 활동 정보를 가져오는 데 실패했습니다.",
-                });
-            }
-
-            const svg = RenderWeeklyActivitySVG(data, reqWidth, reqHeight);
-            res.type('image/svg+xml').send(svg);
-
-        }
-        catch (error) {
-            if (error instanceof CommonError) {
-                res.status(error.status).type('image/svg+xml').send(ErrorSVG(error, 900, 430));
-            }
-            else if (error instanceof Error) {
-                const commonError = new CommonError({
-                    type: "https://docs.github.com/en/graphql/overview/explorer",
-                    title: error.name,
-                    status: 500,
-                    detail: error.message
-                });
-                res.status(500).type('image/svg+xml').send(ErrorSVG(commonError, 900, 430));
-            }
-            else {
-                const error = new CommonError({
-                    type: "https://docs.github.com/en/graphql/overview/explorer",
-                    title: "Unknown Error",
-                    status: 500,
-                    detail: "An unknown error occurred."
-                });
-                res.status(500).type('image/svg+xml').send(ErrorSVG(error, 900, 430));
-            }
-        }
-    }
-
-
+    
     public weeklyCommitActivity = async (req: Request, res: Response) => {
         try {
             const username: string = String(req.query.username ?? "");
             const reqWidth = Number(req.query.width ?? 900);
             const reqHeight = Number(req.query.height ?? 430);
 
-            const data = await repoService.getCommitTime(username, undefined);
-
-            if (!data) {
-                throw new CommonError({
-                    status: 500,
-                    title: "GitHub API Error",
-                    type: "https://docs.github.com/en/graphql/overview/explorer",
-                    detail: "주간 커밋 활동 정보를 가져오는 데 실패했습니다.",
-                });
+            if (!username || username.trim() === "") {
+                throw CommonError.create400Error("Github에서 사용하는 유저 이름을 username 쿼리 파라미터로 전달해 주세요", "/api/readme/weekly-commit-activity.svg");
             }
 
-            const svg = RenderWeeklyActivitySVG(data, reqWidth, reqHeight);
-            res.type('image/svg+xml').send(svg);
+            
+            const svg = await readmeService.weeklyCommitActivity(username, reqWidth, reqHeight);
 
+            res.status(200).type('image/svg+xml').send(svg);
         }
         catch (error) {
-            if (error instanceof CommonError) {
-                res.status(error.status).type('image/svg+xml').send(ErrorSVG(error, 900, 430));
-            }
-            else if (error instanceof Error) {
-                const commonError = new CommonError({
-                    type: "https://docs.github.com/en/graphql/overview/explorer",
-                    title: error.name,
-                    status: 500,
-                    detail: error.message
-                });
-                res.status(500).type('image/svg+xml').send(ErrorSVG(commonError, 900, 430));
-            }
-            else {
-                const error = new CommonError({
-                    type: "https://docs.github.com/en/graphql/overview/explorer",
-                    title: "Unknown Error",
-                    status: 500,
-                    detail: "An unknown error occurred."
-                });
-                res.status(500).type('image/svg+xml').send(ErrorSVG(error, 900, 430));
-            }
+            readmeErrorResponseHandler(error, res);
         }
     }
+    
 
 
     public notFound = (req: Request, res: Response) => {
--- backend/src/services/readme.services.ts
+++ backend/src/services/readme.services.ts
@@ -2,13 +2,17 @@ import { redisClient } from "../infra/redis/redisClient";
 import { RenderCommitActivitySVG } from "../render/RenderCommitActivitySVG";
 import CommonError from "../utils/common-error";
 import contributionService from "./contribution.services";
-import { getUISize, getISOFromTo, getDisplayFromTo, initParamsDate, isValidDateRange } from "../utils/readme.util";
-import { createRedisKey } from "../utils/redis.util";
+import { getUISize, getISOFromTo, getDisplayFromTo, initParamsDate, isValidDateRange, readmeErrorHandler } from "../utils/readme.util";
+import repoService from "./repo.services";
+import RenderTechnologyDistributionSVG from "../render/RenderTechnologyDistributionSVG";
+import RenderPreferredCommitTimeSVG from "../render/RenderPreferredCommitTimeSVG";
+import RenderWeeklyActivitySVG from "../render/RenderWeeklyActivitySVG";
+import redisRepository from "../repository/redis.repository";
 
 
+//TODO : 테스트 시 참고 사항 - 서비스 종속성 : contributionService, repoService
 
 class ReadmeService {
-
 
         public commitActivity =  async (githubUsername: string, reqWidth : number, reqHeight : number, paramsFrom : string | undefined, paramsTo : string | undefined) => {
         
@@ -18,65 +22,106 @@ class ReadmeService {
                 const { displayFrom, displayTo } = getDisplayFromTo(initFrom, initTo);
                 const { isoFrom, isoTo } = getISOFromTo(initFrom, initTo, { fromHour: 0, toHour: 23 });
 
-            
                 if (!isValidDateRange(initFrom, initTo)) {
-                    throw CommonError.create400Error("Invalid date", "/api/readme/commit-activity.svg");
+                    throw CommonError.create400Error("유효하지 않은 날짜 범위입니다.", "/api/readme/commit-activity.svg");
                 }
 
-                const redisKey = createRedisKey(githubUsername, "commitActivity", {
+                const redisKey = redisRepository.createRedisKey(githubUsername, "commitActivity", {
                     additionalKey : "svg",
                     from : initFrom,
                     to : initTo,
                     lastKey : `${width}x${height}`
                 });
-
-                console.log(`Redis Key: ${redisKey}`);
-
                 //어차피 initFrom과 initTo는 YYYYMMDD 형식으로 들어옴
-                const cachedData = await redisClient.get(redisKey);
+                const cachedData = await redisRepository.get<string>(redisKey);
                 if (cachedData) {
-                    const svg = cachedData;
                     console.log("Serving cached SVG for commit activity chart");
-                    return svg;
+                    return cachedData;
                 }
 
-
                 const commitActivity = await contributionService.getCommitActivity(undefined, githubUsername, isoFrom, isoTo);
+                if(!commitActivity){
+                    throw CommonError.create500Error("커밋 활동 정보를 가져오는 데 실패했습니다.", "/api/readme/commit-activity.svg");
+                }
+                
                 const svg = RenderCommitActivitySVG(commitActivity, displayFrom, displayTo, width, height);
-
-
-
-                console.log(`Set Redis Key: ${redisKey}`);
-                await redisClient.setEx(redisKey, 60 * 60, svg); // 캐시 만료 시간: 1시간
+                await redisRepository.setEx(redisKey, 60 * 60, svg); // 캐시 만료 시간: 1시간
                 return svg;
 
             }catch(error){
-
-                if(error instanceof CommonError){
-                    throw error;
-                }
-                else if(error instanceof Error){
-                    throw new CommonError({
-                        type: "https://docs.github.com/en/graphql/overview/explorer",
-                        title: error.name,
-                        status: 500,
-                        detail: error.message
-                    });
-                }
-                else{
-                    throw new CommonError({
-                        type: "https://docs.github.com/en/graphql/overview/explorer",
-                        title: "Unknown Error",
-                        status: 500,
-                        detail: "An unknown error occurred."
-                    });
-                }
-                
-
-
+                readmeErrorHandler(error);
             }
         }
 
+        public techDistribution = async (githubUsername: string, reqWidth : number, reqHeight : number) => {
+            
+            const redisKey = redisRepository.createRedisKey(githubUsername, "techDistribution", {
+                additionalKey : "svg",
+                lastKey : `${reqWidth}x${reqHeight}`
+            });
+
+            const cachedData = await redisRepository.get<string>(redisKey);
+            if (cachedData) {
+                console.log("Serving cached SVG for tech distribution chart");
+                return cachedData;
+            }
+
+            const languageStats = await repoService.getLanguages(githubUsername, undefined);
+            if (!languageStats) {
+                throw CommonError.create500Error("레포지토리 언어 사용량 정보를 가져오는 데 실패했습니다.", "/api/readme/tech-distribution.svg");
+            }
+
+            const svg = RenderTechnologyDistributionSVG(languageStats, reqWidth, reqHeight);
+            await redisRepository.set<string>(redisKey, svg);
+            return svg;
+        }
+
+        
+        public preferredCommitTime = async (githubUsername: string, reqWidth : number, reqHeight : number) => {
+            const redisKey = redisRepository.createRedisKey(githubUsername, "preferredCommitTime", {
+                additionalKey : "svg",
+                lastKey : `${reqWidth}x${reqHeight}`
+            });
+            
+            const cachedData = await redisRepository.get<string>(redisKey);
+            if (cachedData) {
+                console.log("Serving cached SVG for preferred commit time chart");
+                return cachedData;
+            }
+
+            const commitStats = await repoService.getCommitTime(githubUsername, undefined);
+            if (!commitStats) {
+                throw CommonError.create500Error("커밋 시간 정보를 가져오는 데 실패했습니다.", "/api/readme/preferred-commit-time.svg");
+            }
+
+            const svg = RenderPreferredCommitTimeSVG(commitStats, reqWidth, reqHeight);
+            await redisRepository.set<string>(redisKey, svg);
+            return svg;
+
+        }
+
+
+        public weeklyCommitActivity = async (githubUsername: string, reqWidth : number, reqHeight : number) => {
+            const redisKey = redisRepository.createRedisKey(githubUsername, "weeklyCommitActivity", {
+                additionalKey : "svg",
+                lastKey : `${reqWidth}x${reqHeight}`
+            });
+            const cachedData = await redisRepository.get<string>(redisKey);
+            if (cachedData) {
+                console.log("Serving cached SVG for weekly commit activity chart");
+                return cachedData;
+            }
+
+            const commitStats = await repoService.getCommitTime(githubUsername, undefined);
+
+            if (!commitStats) {
+                throw CommonError.create500Error("주간 커밋 활동 정보를 가져오는 데 실패했습니다.", "/api/readme/weekly-commit-activity.svg");
+            }
+
+            const svg = RenderWeeklyActivitySVG(commitStats, reqWidth, reqHeight);
+            await redisRepository.set<string>(redisKey, svg);
+            return svg;
+        }
 
 }
 
